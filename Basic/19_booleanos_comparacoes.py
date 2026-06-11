b92098cf206140e08ee82c9eaca1792316d03785
pedrohlsa <stguts25@gmail.com>
2026-06-11 04:55:48 -0300

    Add boolean comparisons examples in Python

---
+++ Basic/19_booleanos_comparacoes.py
@@ -0,0 +1,20 @@
+# BOOLEANS: True ou False (Case-sensitive!)
+
+# Comparações retornam booleanos
+print(10 > 9)   # True
+print(10 == 9)  # False
+print(10 < 9)   # False
+print(10 != 9)  # True
+
+# Em condições
+a = 200
+b = 33
+
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")  # Vai imprimir isso
+
+# Outros exemplos
+print(5 >= 5)   # True
+print(3 <= 2)   # False
