49ea4f93c4c7c84a9d72f102ebdb9efe51ea719b
pedrohlsa <stguts25@gmail.com>
2026-06-11 00:56:34 -0300

    Add basic print statements in Python

---
+++ Basic/01_hello_world_e_print_basico.py
@@ -0,0 +1,11 @@
+print("Hello, World!")
+
+print("Hello World!", end=" ")
+print("I will print on the same line.")
+
+print(333334)
+print(3 + 3)
+print("3 + 3")
+
+print("I am", 35, "years old")
+print("Ok")  # comentário no final
