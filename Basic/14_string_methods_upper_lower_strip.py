bb9839011cf2e869c72da253ab768361e79ccd7b
pedrohlsa <stguts25@gmail.com>
2026-06-11 04:52:47 -0300

    Add string methods examples for upper, lower, and strip

---
+++ Basic/14_string_methods_upper_lower_strip.py
@@ -0,0 +1,13 @@
+# UPPER() - Tudo maiúsculo
+a = "Hello World!"
+print(a.upper())  # HELLO WORLD!
+
+# LOWER() - Tudo minúsculo
+print(a.lower())  # hello world!
+
+# STRIP() - Remove espaços das pontas
+a = " Hello, World! "
+print(a.strip())  # "Hello, World!"
+
+# lstrip() - remove só da esquerda
+# rstrip() - remove só da direita
