9e52a73bd8b3b4484aab20738531bade8d454ff9
pedrohlsa <stguts25@gmail.com>
2026-06-11 00:58:43 -0300

    Create 08_tipos_de_dados_completos.py

---
+++ Basic/08_tipos_de_dados_completos.py
@@ -0,0 +1,30 @@
+# Texto
+x = "Hello World"
+
+# Números
+x = 20        # int
+x = 20.5      # float
+x = 1j        # complex
+
+# Sequências
+x = ["Apple", "Banana", "Cherry"]  # list
+x = ("Apple", "Banana", "Cherry")  # tuple
+x = range(6)                        # range
+
+# Mapeamento
+x = {"name": "John", "age": 36}    # dict
+
+# Sets
+x = {"apple", "banana", "cherry"}          # set
+x = frozenset({"apple", "banana", "cherry"}) # frozenset
+
+# Boolean
+x = True      # CASE SENSITIVE!
+
+# Binários
+x = b"Hello"
+x = bytearray(5)
+x = memoryview(bytes(5))
+
+# None
+x = None
