3ab96b9dc72da5fcd15007c024aad51183ceebfa
pedrohlsa <stguts25@gmail.com>
2026-06-11 00:56:49 -0300

    Add basic variables and type examples in Python

---
+++ Basic/02_variaveis_e_tipos_basicos.py
@@ -0,0 +1,20 @@
+x = 5
+y = "John"
+
+print(type(x))
+print(type(y))
+
+# Variável muda de tipo
+x = "Jon"
+y = 4
+
+# Type casting
+x = str(3)
+y = int(5)
+z = float(3)
+
+# Case-sensitive
+a = 'John'
+A = 'Pork'
+print(a)
+print(A)
