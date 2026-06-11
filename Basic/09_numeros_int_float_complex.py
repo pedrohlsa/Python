abc6058c3bb2ccb018f1a7058c7d3b0468c23ce6
pedrohlsa <stguts25@gmail.com>
2026-06-11 00:58:53 -0300

    Add examples of integers, floats, and complex numbers

---
+++ Basic/09_numeros_int_float_complex.py
@@ -0,0 +1,16 @@
+# Inteiros (tamanho ilimitado)
+x = 1
+y = 35656222554887711
+z = -3255522
+
+# Floats (com notação científica)
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+# Complexos (com 'j')
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x), type(y), type(z))
