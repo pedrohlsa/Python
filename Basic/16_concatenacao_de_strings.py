ae5eb8c3c59d6931371b336ff086b4889b014adc
pedrohlsa <stguts25@gmail.com>
2026-06-11 04:53:42 -0300

    Add string concatenation examples in Python

---
+++ Basic/16_concatenacao_de_strings.py
@@ -0,0 +1,15 @@
+# CONCATENAÇÃO COM +
+a = "Hello"
+b = "World"
+c = a + b
+print(c)  # HelloWorld
+
+# Adicionando espaço
+d = a + " " + b
+print(d)  # Hello World
+
+# Múltiplas strings
+nome = "João"
+sobrenome = "Silva"
+nome_completo = nome + " " + sobrenome
+print(nome_completo)  # João Silva
