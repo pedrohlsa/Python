1a3e477586b9eece2360d9c0f71c7d5d9afd5e63
pedrohlsa <stguts25@gmail.com>
2026-06-11 00:59:07 -0300

    Create 10_type_conversion_casting.py

---
+++ Basic/10_type_conversion_casting.py
@@ -0,0 +1,18 @@
+# Conversão automática
+x = 1      # int
+y = 2.8    # float
+z = 1j     # complex
+
+a = float(x)   # int -> float
+b = int(y)     # float -> int
+c = complex(x) # int -> complex
+
+# Casting com funções construtoras
+x = int(1)      # 1
+y = int(2.8)    # 2
+z = int("3")    # 3
+
+x = float(1)    # 1.0
+y = float(2.8)  # 2.8
+z = float("3")  # 3.0
+w = float("4.2") # 4.2
