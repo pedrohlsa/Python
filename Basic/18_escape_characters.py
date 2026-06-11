23063b45fc0dbe68dcc5f60c136c1159ffe01e0d
pedrohlsa <stguts25@gmail.com>
2026-06-11 04:55:36 -0300

    Create 18_escape_characters.py

---
+++ Basic/18_escape_characters.py
@@ -0,0 +1,24 @@
+# ASPAS DUPLAS dentro de string (\" )
+txt = "We are the so-called \"Vikings\" from the north"
+print(txt)  # We are the so-called "Vikings" from the north
+
+# ASPAS SIMPLES (\' )
+txt = 'It\'s a beautiful day'
+print(txt)  # It's a beautiful day
+
+# BACKLASH LITERAL (\\ )
+txt = "C:\\Users\\Nome\\Desktop"
+print(txt)  # C:\Users\Nome\Desktop
+
+# NOVA LINHA (\n)
+print("Linha 1\nLinha 2")
+
+# TAB (\t)
+print("Nome:\tJoão")
+
+# Backspace (\b) - apaga caractere anterior
+print("Hello\bWorld")  # HellWorld
+
+# Octal (\ooo) e Hexa (\xhh)
+print("\110\145\154\154\157")  # Hello (valores octais)
+print("\x48\x65\x6c\x6c\x6f")  # Hello (valores hexa)
