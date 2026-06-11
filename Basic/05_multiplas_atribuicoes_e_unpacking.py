a589e4e6d8954fef04f9557073dbc1a870b888f2
pedrohlsa <stguts25@gmail.com>
2026-06-11 00:57:23 -0300

    Add examples of multiple assignments and unpacking

---
+++ Basic/05_multiplas_atribuicoes_e_unpacking.py
@@ -0,0 +1,12 @@
+# Múltiplas em uma linha
+k, l, y = "Orange", "Banana", "Cherry"
+print(k, l, y)
+
+# Unpacking de lista
+fruits = ["Apple", "Pearl", "Juice"]
+q, t, r = fruits
+print(q, t, r)
+
+# Todas com mesmo valor
+q = t = r = "Apple"
+print(q, t, r)
