1a30b4910313385d98dcb4ed47c5fe836fdc82ee
pedrohlsa <stguts25@gmail.com>
2026-06-14 19:08:14 -0300

    Create 48_loop_em_sets.py

---
+++ Basic/48_loop_em_sets.py
@@ -0,0 +1,9 @@
+# LOOP em sets (única forma de acessar itens)
+
+thisset = {"apple", "banana", "cherry"}
+
+# For loop direto
+for x in thisset:
+    print(x)
+
+# Sets são desordenados, então a cada execução pode vir em ordem diferente
