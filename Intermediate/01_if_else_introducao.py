6a479bf00b1efbee78bb66d4b699c216cb8fda7b
pedrohlsa <stguts25@gmail.com>
2026-06-18 23:54:40 -0300

    Add introductory example for if/else statements

---
+++ Intermediate/01_if_else_introducao.py
@@ -0,0 +1,24 @@
+# IF/ELSE - Introdução
+
+# Estrutura básica
+a = 33
+b = 200
+
+if b > a:
+    print("b is greater than a")
+
+# Condição que retorna True/False
+number = 15
+if number > 0:
+    print("The number is positive")
+
+# Múltiplas instruções (mesmo nível de indentação)
+age = 20
+if age >= 18:
+    print("You are an adult")
+    print("You can vote")
+
+# Variável booleana
+is_logged_in = True
+if is_logged_in:
+    print("Welcome back")
