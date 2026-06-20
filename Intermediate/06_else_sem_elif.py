bb919fa4bf071805ed17bd03ca24f21e906e4e35
pedrohlsa <stguts25@gmail.com>
2026-06-20 01:22:04 -0300

    Add example of else statement in Python

---
+++ Intermediate/06_else_sem_elif.py
@@ -0,0 +1,19 @@
+# ELSE sem ELIF - Fallback padrão
+
+a = 200
+b = 33
+
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
+
+# Exemplo prático: par ou ímpar
+number = 7
+
+if number % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
+
+# ELSE sempre vem por ÚLTIMO
